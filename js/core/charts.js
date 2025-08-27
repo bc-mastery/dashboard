6eb015e5f3e1803d108cefa4814506d049a04244
bc-mastery <bc@businesscanvas.io>
2025-08-27 14:50:50 +0200

    Update charts.js

--- js/core/charts.js
+++ js/core/charts.js
@@ -130,7 +130,7 @@ export function centerLockChart({
   wrapper,
   host,
   extraYOffset = 0,
-  mobileYOffset = -12,
+  mobileYOffset = -20,
 }) {
   if (!wrapper || !host) return;
 
@@ -215,3 +215,4 @@ export function nudgeChartY(hostEl, px = 0) {
   const mo = new MutationObserver(apply);
   if (hostEl) mo.observe(hostEl, { childList: true, subtree: true });
 }
+
