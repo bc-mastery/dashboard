7bf51b3fd8df864bd1a7cfcfbcb4693f4d1f7755
bc-mastery <bc@businesscanvas.io>
2025-08-30 16:52:11 +0200

    Update targeting.js

--- js/pages/targeting.js
+++ js/pages/targeting.js
@@ -77,7 +77,7 @@ function injectTargetingStylesOnce() {
       }
       /* Mobile-only fine-tune: lift donut a few px (negative = up) */
       #content .bfMap .abc-wrap .donut {
-        --donut-nudge-y: 0px; /* try -6 / -10 to taste */
+        --donut-nudge-y: 0px;
       }
     }
   `;
@@ -241,4 +241,5 @@ function paintTargeting(api, allowFull = false) {
     }
   });
 }
+
 
