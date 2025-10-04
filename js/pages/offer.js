e602bfa2f6e11e19f4244818f6f66a03dc7676d8
bc-mastery <bc@businesscanvas.io>
2025-10-04 22:38:42 +0200

    Update offer.js

--- js/pages/offer.js
+++ js/pages/offer.js
@@ -84,19 +84,73 @@ function paintOffer(api, allowFull = false) {
     subtitleValue: d.O_CHARACTER,
     descText: d.O_CHARACTER_DESC,
     areas,
+    page: "offer",
   });
 
   if (allowFull) {
     html += `
-      <div class="card scrollTarget" id="block-offer-details">
-        <div class="sectionTitle">Offer Strategy</div>
-        ${d.O_CONCEPT ? `<p><span class="subtitle">Core Concept:</span> ${esc(d.O_CONCEPT)}</p>` : ""}
-        ${d.O_CHARACTERISTICS ? `<p><span class="subtitle">Key Characteristics:</span> ${esc(d.O_CHARACTERISTICS)}</p>` : ""}
-        ${d.O_FEATURE ? `<p><span class="subtitle">Defining Feature:</span> ${esc(d.O_FEATURE)}</p>` : ""}
-        ${d.O_VALUE ? `<p><span class="subtitle">Primary Value:</span> ${esc(d.O_VALUE)}</p>` : ""}
-        ${d.O_RETENTION ? `<p><span class="subtitle">Retention Mechanism:</span> ${esc(d.O_RETENTION)}</p>` : ""}
-        ${d.O_APPEARANCE ? `<p><span class="subtitle">Appearance and Delivery:</span> ${esc(d.O_APPEARANCE)}</p>` : ""}
-        ${d.O_PRICING ? `<p><span class="subtitle">Pricing Strategy:</span> ${esc(d.O_PRICING)}</p>` : ""}
+      <div class="card scrollTarget" id="block-concept">
+        <div class="sectionTitle">Concept</div>
+        <p><span class="subtitle">Offer Character:</span><br>${esc(d.O_CHARACTER_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CONCEPT_FACTOR_1)}</span><br>${esc(d.O_CONCEPT_FACTOR_1_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CONCEPT_FACTOR_2)}</span><br>${esc(d.O_CONCEPT_FACTOR_2_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CONCEPT_FACTOR_3)}</span><br>${esc(d.O_CONCEPT_FACTOR_3_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CONCEPT_FACTOR_4)}</span><br>${esc(d.O_CONCEPT_FACTOR_4_DESC)}</p>
+      </div>
+
+      <div class="card scrollTarget" id="block-characteristics">
+        <div class="sectionTitle">Characteristics</div>
+        <p><span class="subtitle">${esc(d.O_CHARACTERISTIC_1)}</span><br>${esc(d.O_CHARACTERISTIC_1_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CHARACTERISTIC_2)}</span><br>${esc(d.O_CHARACTERISTIC_2_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CHARACTERISTIC_3)}</span><br>${esc(d.O_CHARACTERISTIC_3_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CHARACTERISTIC_4)}</span><br>${esc(d.O_CHARACTERISTIC_4_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CHARACTERISTIC_5)}</span><br>${esc(d.O_CHARACTERISTIC_5_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CHARACTERISTIC_6)}</span><br>${esc(d.O_CHARACTERISTIC_6_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_CHARACTERISTIC_7)}</span><br>${esc(d.O_CHARACTERISTIC_7_DESC)}</p>
+      </div>
+
+      <div class="card scrollTarget" id="block-features">
+        <div class="sectionTitle">Features and Services</div>
+        <p><span class="subtitle">${esc(d.O_FEATURE_1)}</span><br>${esc(d.O_FEATURE_1_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_FEATURE_2)}</span><br>${esc(d.O_FEATURE_2_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_FEATURE_3)}</span><br>${esc(d.O_FEATURE_3_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_FEATURE_4)}</span><br>${esc(d.O_FEATURE_4_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_FEATURE_5)}</span><br>${esc(d.O_FEATURE_5_DESC)}</p>
+      </div>
+
+      <div class="card scrollTarget" id="block-value-triggers">
+        <div class="sectionTitle">Value Triggers</div>
+        <p><span class="subtitle">${esc(d.O_VALUE_1)}</span><br>${esc(d.O_VALUE_1_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_VALUE_2)}</span><br>${esc(d.O_VALUE_2_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_VALUE_3)}</span><br>${esc(d.O_VALUE_3_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_VALUE_4)}</span><br>${esc(d.O_VALUE_4_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_VALUE_5)}</span><br>${esc(d.O_VALUE_5_DESC)}</p>
+      </div>
+
+      <div class="card scrollTarget" id="block-retention">
+        <div class="sectionTitle">Retention Factors</div>
+        <p><span class="subtitle">${esc(d.O_RETENTION_1)}</span><br>${esc(d.O_RETENTION_1_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_RETENTION_2)}</span><br>${esc(d.O_RETENTION_2_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_RETENTION_3)}</span><br>${esc(d.O_RETENTION_3_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_RETENTION_4)}</span><br>${esc(d.O_RETENTION_4_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_RETENTION_5)}</span><br>${esc(d.O_RETENTION_5_DESC)}</p>
+      </div>
+
+      <div class="card scrollTarget" id="block-appearance">
+        <div class="sectionTitle">Appearance</div>
+        <p><span class="subtitle">${esc(d.O_APPEARANCE_1)}</span><br>${esc(d.O_APPEARANCE_1_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_APPEARANCE_2)}</span><br>${esc(d.O_APPEARANCE_2_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_APPEARANCE_3)}</span><br>${esc(d.O_APPEARANCE_3_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_APPEARANCE_4)}</span><br>${esc(d.O_APPEARANCE_4_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_APPEARANCE_5)}</span><br>${esc(d.O_APPEARANCE_5_DESC)}</p>
+      </div>
+
+      <div class="card scrollTarget" id="block-pricing">
+        <div class="sectionTitle">Pricing</div>
+        <p><span class="subtitle">${esc(d.O_PRICING_POSITIONING)}</span><br>${esc(d.O_PRICING_POSITIONING_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_PRICING_PRICE_POINT)}</span><br>${esc(d.O_PRICING_PRICE_POINT_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_PRICING_PRICING_LOGIC)}</span><br>${esc(d.O_PRICING_PRICING_LOGIC_DESC)}</p>
+        <p><span class="subtitle">${esc(d.O_PRICING_FRICTION_REDUCTION)}</span><br>${esc(d.O_PRICING_FRICTION_REDUCTION_DESC)}</p>
       </div>
     `;
   }
